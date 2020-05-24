24b715b0f619f0b53070753aacdea8683eba37ea
Wylliss <wyllissilva@gmail.com>
2020-05-24 18:11:20 -0300

    atualizando atividades

--- helloSpringBoot/src/main/java/br/usjt/helloSpringBoot/repository/AlunosRepository.java
+++
@@ -1,9 +0,0 @@
-package br.usjt.helloSpringBoot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository; 
-
-import br.usjt.helloSpringBoot.model.Aluno;
-
-public interface AlunosRepository extends JpaRepository<Aluno, Long>{
-
-}
--- helloSpringBoot/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-insert into aluno (id, nome, media_notas) values (1, 'Ana', 10);
-insert into aluno (id, nome, media_notas) values (2, 'Maria', 3);
-insert into aluno (id, nome, media_notas) values (3, 'Ricardo', 7);
